6dedc938a88461817b1993bc9aa289d32ed95ad7
Ohleesang <jammmmed@gmail.com>
2023-11-21 11:43:20 +0900

    Map 이용 시간복잡도 O(n)+O(m)+O(nlogn)

--- src/main/kotlin/Solution.kt
+++ src/main/kotlin/Solution.kt
@@ -3,13 +3,15 @@
  *
  * 해설진들은 선수들이 자기 바로 앞의 선수를 추월할 때 추월한 선수 이름을 부른다.
  */
-
+/**
+ *  Map 이용 시간복잡도 O(n)+O(m)+O(nlogn)
+ */
 class Solution {
     fun solution(players: Array<String>, callings: Array<String>): Array<String> {
         var answer: Array<String> = arrayOf<String>()
-
+/*      맵은 효율적이지 못한것같다..
         var maps = players.associateBy({it},{players.indexOf(it)+1}).toMutableMap()
-        //자료구조 맵 으로 저장 맵(이름,순위)
+        //자료구조 맵 으로 저장 맵(이름,순위) x
         var num = 0
         for(i in callings.indices){//해설진이 부른 선수들 위치를 변환
             num = maps.get(callings[i])!!//부른 선수의 순위를 구함
@@ -20,7 +22,10 @@ class Solution {
         }
         maps = maps.toSortedMap(compareBy({maps[it]}))// 맵의 값들을 비교하여 정렬
         answer = maps.keys.toTypedArray()//key값(이름)을 따로 저장
+*/
+
         return answer
+
     }
 }
 
